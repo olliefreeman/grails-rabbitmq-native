81da1d4379e130dc4e52b930500614127854b6bb
Ollie Freeman <oliver.freeman@ndm.ox.ac.uk>
2016-12-05 14:04:36 +0000

    Update the unit test to stub the getFullName method

--- src/test/groovy/com/budjb/rabbitmq/test/consumer/ConsumerManagerImplSpec.groovy
+++ src/test/groovy/com/budjb/rabbitmq/test/consumer/ConsumerManagerImplSpec.groovy
@@ -78,10 +78,10 @@ class ConsumerManagerImplSpec extends Specification {
     def 'Test load/registering of consumer artefacts'() {
         setup:
         GrailsClass artefact1 = Mock(GrailsClass)
-        artefact1.getPropertyName() >> 'consumer1'
+        artefact1.getFullName() >> 'consumer1'
 
         GrailsClass artefact2 = Mock(GrailsClass)
-        artefact2.getPropertyName() >> 'consumer2'
+        artefact2.getFullName() >> 'consumer2'
 
         Consumer1 consumer1 = new Consumer1()
         Consumer2 consumer2 = new Consumer2()
@@ -308,11 +308,11 @@ class ConsumerManagerImplSpec extends Specification {
         grailsApplication.getConfig() >> new PropertySourcesConfig()
 
         GrailsClass artefact1 = Mock(GrailsClass)
-        artefact1.getPropertyName() >> 'unitTestConsumer'
+        artefact1.getFullName() >> 'unitTestConsumer'
         artefact1.getShortName() >> 'UnitTestConsumer'
 
         GrailsClass artefact2 = Mock(GrailsClass)
-        artefact2.getPropertyName() >> 'missingConfigurationConsumer'
+        artefact2.getFullName() >> 'missingConfigurationConsumer'
         artefact2.getShortName() >> 'MissingConfigurationConsumer'
 
         UnitTestConsumer consumer1 = new UnitTestConsumer()
